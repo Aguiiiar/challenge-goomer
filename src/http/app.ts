6a662ad790431f510b196c6487d98dea33e41d16
Aguiar <devaguiar14@gmail.com>
2023-03-04 18:32:30 -0300

    feat: tratamento de erros

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,10 +1,23 @@
-import fastify from 'fastify';
-import { NotFound } from 'http-errors'
+import fastify, { errorCodes } from 'fastify';
+import { NotFound, InternalServerError } from 'http-errors'
 
-const app = fastify();
+const app = fastify({
+  logger: true
+});
 
-app.get('/*', async (request, reply) => {
+app.get('/*', async (_request, _reply) => {
   return NotFound('Route not found');
+});
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof errorCodes.FST_ERR_BAD_STATUS_CODE) {
+
+    // app.log.error(error)
+
+    return new InternalServerError();
+  } else {
+    reply.send(error);
+  }
 })
 
-export default app
+export default app;
